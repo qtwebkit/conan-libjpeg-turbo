0f72c68697028699abd131b91c6578c3709ca7ad
theirix <theirix@gmail.com>
2018-01-21 21:34:40 +0300

    Proper build_requires order

--- conanfile.py
+++ conanfile.py
@@ -23,8 +23,8 @@ class LibJpegTurboConan(ConanFile):
     
     def build_requirements(self):
         if self.settings.os == "Windows" and self.settings.compiler != "Visual Studio":
-            self.build_requires("msys2_installer/latest@bincrafters/stable")
             self.build_requires("mingw_installer/1.0@conan/stable")
+            self.build_requires("msys2_installer/latest@bincrafters/stable")
 
     def config(self):
         del self.settings.compiler.libcxx 
