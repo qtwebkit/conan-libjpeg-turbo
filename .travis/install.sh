b5225f26aa927626578af5d86292c60b4f730fda
theirix <theirix@gmail.com>
2018-02-28 15:44:22 +0300

    Brought back travis macOS builds

--- .travis/install.sh
+++ .travis/install.sh
@@ -5,10 +5,21 @@ set -x
 
 if [[ "$(uname -s)" == 'Darwin' ]]; then
     brew update || brew update
+    brew outdated pyenv || brew upgrade pyenv
+    brew install pyenv-virtualenv
     brew install cmake || true
     brew install nasm || true
+
+    if which pyenv > /dev/null; then
+        eval "$(pyenv init -)"
+    fi
+
+    pyenv install 2.7.10
+    pyenv virtualenv 2.7.10 conan
+    pyenv rehash
+    pyenv activate conan
 else
-	sudo apt-get install -y nasm autoconf dh-autoreconf
+    sudo apt-get install -y nasm autoconf dh-autoreconf
 fi
 
 pip install conan --upgrade
