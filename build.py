cfd7ea7b6bb1c4fe46e13a34ad2d159476104f56
Konstantin Tokarev <annulen@yandex.ru>
2019-07-17 01:01:27 +0300

    Remove weird code from build.py, build only static lib

--- build.py
+++ build.py
@@ -11,20 +11,8 @@ if __name__ == "__main__":
 
     items = []
     for item in builder.items:
-        # skip mingw cross-builds
-        if not (platform.system() == "Windows" and item.settings["compiler"] == "gcc" and
-                item.settings["arch"] == "x86"):
-            new_build_requires = copy.copy(item.build_requires)
-            # add msys2 and mingw as a build requirement for mingw builds
-            if platform.system() == "Windows" and item.settings["compiler"] == "gcc":
-                new_build_requires["*"] = new_build_requires.get("*", []) + \
-                    ["mingw_installer/1.0@conan/stable",
-                     "msys2_installer/latest@bincrafters/stable"]
-                items.append([item.settings, item.options, item.env_vars,
-                              new_build_requires, item.reference])
-            else:
-                # or just add build
-                items.append(item)
+        if item.options["libjpeg-turbo:shared"] == False:
+            items.append(item)
     builder.items = items
 
     builder.run()
